52d18c900692837dc86198359873b8a97f56b60d
migalko94 <miguelgalvezugr@gmail.com>
2024-01-15 20:18:37 +0100

    encapsulated middlewares

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,19 @@
 import "#core/load-env.js";
-import express from "express";
+import {
+  logErrorRequestMiddleware,
+  logRequestMiddleware,
+} from "#common/middlewares/logger.middlewares.js";
 import { createRestApiServer } from "#core/servers/index.js";
 import { envConstants } from "#core/constants/index.js";
 import { housesApi } from "#pods/house/index.js";
 
 const restApiServer = createRestApiServer();
 
-restApiServer.use(async (req, res, next) => {
-  console.log(req.url);
-  next();
-});
+restApiServer.use(logRequestMiddleware);
 
 restApiServer.use("/api/houses", housesApi);
 
-restApiServer.use(async (error, req, res, next) => {
-  console.error(error);
-  res.sendStatus(500);
-});
+restApiServer.use(logErrorRequestMiddleware);
 
 restApiServer.listen(envConstants.PORT, () => {
   console.log(`Server ready at port ${envConstants.PORT}`);
